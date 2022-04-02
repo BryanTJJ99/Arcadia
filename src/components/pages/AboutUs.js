c7a41911ae2d093a38f0f8a9bd40ba77d0281ae0
Jin Mei Xuan <jin.meixuan02@gmail.com>
2022-04-02 22:42:02 +0800

    Implement NFT minting

--- src/components/pages/AboutUs.js
+++ src/components/pages/AboutUs.js
@@ -2,18 +2,11 @@ import React from 'react';
 import '../../App.css';
 
 function AboutUs() {
-  // return <div style={{  
-  //   backgroundImage: `url(require("images/arcadia.jpg))`,
-  //   backgroundPosition: 'center',
-  //   backgroundSize: 'cover',
-  //   backgroundRepeat: 'no-repeat'
-  // }}>
-  return <div>
-    <h1 className='aboutus'>
-      About Us
-    </h1>
-    <img src='images/arcadia.jpg'
-      style={{width:"3000", height:"3000", flex:"1"}}></img>
+  return <div className='aboutus'>
+    <h1>What is Arcadia?</h1>
+    <p>Associated with bountiful natural splendor and harmony, Arcadia (Greek: Αρκαδία) is a poetic byword 
+      for an idyllic vision of unspoiled wilderness, with the vision of pastoralism and harmony with nature.
+    </p>
   </div>
 
     ;
